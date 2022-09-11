1f983b792ab217fc9c72824f2718b83517cf7ceb
Korapin <korapin.6945@mail.kmutt.ac.th>
2022-09-11 13:25:00 +0700

    BMI

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return weight/(height*height)
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  if(calculateBMI(weight, height) < 18.5){
+    return "Underweight"
+  }else if (calculateBMI(weight, height) > 18.5 && calculateBMI(weight, height) < 24.9){
+    return "Normal weight"
+  }else if (calculateBMI(weight, height) > 25.0){
+  return "Overweight"
 }
+}
+
+
 module.exports = getBMIMeaning
+
+
